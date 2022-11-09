b591e6b58eb00c2fdb359acb8ae17cb7fc5b058f
Teddy Atkinson <tedddyatkinson@gmail.com>
2022-11-09 12:03:56 -0800

    Pass test for opponent class constructor

--- __tests__/RPG.test.js
+++ __tests__/RPG.test.js
@@ -1,8 +1,41 @@
-import {Character} from "./../src/RPG.js"
+import { Character, Opponent } from "./../src/RPG.js"
+
 
 describe("Character", () => {
-  test("Should create a new character class object", () => {
-    let player1 = new Character(0);
-    expect(player1.strength).toEqual(0);
+  test("Should create a new character class object with name 'Beatrix' and default stats", () => {
+    let player1 = new Character("Beatrix");
+    expect(player1.name).toEqual("Beatrix");
+    expect(player1.strength).toEqual(2);
+    expect(player1.defense).toEqual(2);
+    expect(player1.xp).toEqual(0);
+    expect(player1.health).toEqual(100);
+    expect(player1.charLevel).toEqual(1);
+    expect(player1.inventory).toEqual([]);
+
+
+  });
+
+  test("Should add 1 level when xp reaches over 100", () => {
+    let player1 = new Character("Bo");
+    let player2 = new Character("Beatrix");
+    player1.xp = 100;
+    player1.levelUp();
+    player2.xp = 50;
+    expect(player1.charLevel).toEqual(2);
+    expect(player2.charLevel).toEqual(1);
+    expect(player1.xp).toEqual(0);
+    expect(player2.xp).toEqual(50);
+  });
+
+});
+
+describe("Opponent", () => {
+  test("should create a new opponent object", () => {
+    let highRob = new Opponent("Highway Robber", 2, 5);
+    expect(highRob.name).toEqual("Highway Robber");
+    expect(highRob.strength).toEqual(2);
+    expect(highRob.health).toEqual(5);
+    expect(highRob.xpReward).toEqual(5);
   })
+
 })
--- src/RPG.js
+++ src/RPG.js
@@ -1,6 +1,39 @@
+export class Game {
+  constructor (playerName) {
+    this.playerName = playerName
+  }
+
+  battle(opponent) {
+    
+  }
+
+}
+
+
 export class Character {
-  constructor(strength){
+  constructor(name){
+    this.charLevel = 1;
+    this.name = name;
+    this.strength = this.charLevel * 2;
+    this.defense = this.charLevel * 2;
+    this.xp = 0;
+    this.health = 100;
+    this.inventory = [];
+  }
+  levelUp() {
+    if (this.xp >= 100) {
+      this.xp = this.xp - 100;
+      this.charLevel += 1;
+      this.health = 100;
+    }
+  }
+}
+
+export class Opponent {
+  constructor (name, strength, health) {
+    this.name = name;
     this.strength = strength;
-    // this.defense = defense;
+    this.health = health;
+    this.xpReward = health;
   }
 }
