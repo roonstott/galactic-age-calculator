5864d93caee150525aa87f980f76f63d176113a6
Robert Onstott <roonstott@gmail.com>
2022-11-14 12:30:47 -0800

    Pass jupiterAge test

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -6,6 +6,7 @@ export class Person {
     this.mercuryAge = Math.floor(this.earthAge/0.24);
     this.venusAge = Math.floor(this.earthAge/0.62);
     this.marsAge = Math.floor(this.earthAge/1.88);
+    this.jupiterAge = Math.floor(this.earthAge/11.86);
   }
 }
 
