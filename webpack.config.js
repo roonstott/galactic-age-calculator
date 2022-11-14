c129334ca9c13ddbef1f3e1a5ae3b463cf72695b
Robert Onstott <roonstott@gmail.com>
2022-11-14 08:10:49 -0800

    Clone working envirnoment from prior project and rename files

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'RPG',
+      title: 'galactic-age',
       template: './src/index.html',
       inject: 'body'
     })
