36fefc054ccb8c17f23ac838e3c66b9c8e24756d
Robert Onstott <roonstott@gmail.com>
2022-11-14 12:26:56 -0800

    Pass the marsAge test

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -5,6 +5,7 @@ export class Person {
   galacticAge() {
     this.mercuryAge = Math.floor(this.earthAge/0.24);
     this.venusAge = Math.floor(this.earthAge/0.62);
+    this.marsAge = Math.floor(this.earthAge/1.88);
   }
 }
 
